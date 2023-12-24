4afe9af79da99403ece883132ba2ad6cb0230ad6
nikita3006 <nikitanegi3011@gmail.com>
2023-12-24 22:44:54 +0530

    Adding Network Calls Task-20

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,65 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import { Fragment, useEffect } from 'react';
+import { uiActions } from './store/ui-slice';
+import Notification from './components/UI/Notification';
 
+let isInitial = true;
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector(state=> state.ui.cartIsVisible);
+  const cart = useSelector(state => state.cart);
+  const notification = useSelector(state => state.ui.notification);
+
+  useEffect(()=>{
+    const sendCartData = async()=>{
+      dispatch(uiActions.showNotification({
+        status: 'pending',
+        title : 'Sending',
+        message : 'Sending cart data!',
+      }))
+      const response = await fetch('https://expense-tracker-10oct-default-rtdb.firebaseio.com/cart.json',{
+        method:'PUT',
+        body: JSON.stringify(cart),
+      });
+
+      if(!response.ok){
+        throw new Error('sending data failed');
+      }
+
+      const reponseData = await response.json();
+
+      dispatch(uiActions.showNotification({
+        status: 'success',
+        title : 'Success!',
+        message : 'Sent cart data successfully',
+      }))
+    }
+
+    if(isInitial){
+      isInitial= false;
+      return;
+    }
+    sendCartData().catch(error => {
+      dispatch(uiActions.showNotification({
+        status: 'success',
+        title : 'Success!',
+        message : 'Sent cart data successfully',
+      }))
+    })
+
+  },[cart])
+
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && <Notification status={notification.status} title={notification.title} message={notification.message} />}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
