ce789fc210fa7394a07426eed54b97b172cf8c15
nikita3006 <nikitanegi3011@gmail.com>
2023-12-24 22:45:45 +0530

    art problems fixed and completed (task-21)

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Products from './components/Shop/Products';
 import { Fragment, useEffect } from 'react';
 import { uiActions } from './store/ui-slice';
 import Notification from './components/UI/Notification';
+import { sendCartData, fetchCartData } from './store/cart-Action';
 
 let isInitial = true;
 function App() {
@@ -14,41 +15,18 @@ function App() {
   const notification = useSelector(state => state.ui.notification);
 
   useEffect(()=>{
-    const sendCartData = async()=>{
-      dispatch(uiActions.showNotification({
-        status: 'pending',
-        title : 'Sending',
-        message : 'Sending cart data!',
-      }))
-      const response = await fetch('https://expense-tracker-10oct-default-rtdb.firebaseio.com/cart.json',{
-        method:'PUT',
-        body: JSON.stringify(cart),
-      });
+    dispatch(fetchCartData());
+  },[])
 
-      if(!response.ok){
-        throw new Error('sending data failed');
-      }
-
-      const reponseData = await response.json();
-
-      dispatch(uiActions.showNotification({
-        status: 'success',
-        title : 'Success!',
-        message : 'Sent cart data successfully',
-      }))
-    }
-
+  useEffect(()=>{
     if(isInitial){
       isInitial= false;
       return;
     }
-    sendCartData().catch(error => {
-      dispatch(uiActions.showNotification({
-        status: 'success',
-        title : 'Success!',
-        message : 'Sent cart data successfully',
-      }))
-    })
+   
+    if(cart.changed===true){
+      dispatch(sendCartData(cart));
+    }
 
   },[cart])
 
